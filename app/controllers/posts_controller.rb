36d6d7962f64fa8cec32b40a2bbe27e2378fec21
Aiden <aiden.marques94@gmail.com>
2021-04-15 19:36:46 -0400

    commit tree 674448eb43e50a222d040a33ea362021237df0f9

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,8 +17,14 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
-  end
+    def new
+      if params[:author_id] && !Author.exists?(params[:author_id])
+        redirect_to authors_path, alert: "Author not found."
+      else
+        @post = Post.new(author_id: params[:author_id])
+      end
+    end 
+  end 
 
   def create
     @post = Post.new(post_params)
@@ -26,19 +32,29 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
-  def update
-    @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to post_path(@post)
-  end
-
-  def edit
-    @post = Post.find(params[:id])
-  end
+    def update
+      @post = Post.find(params[:id])
+      @post.update(post_params)
+      redirect_to post_path(@post)
+    end 
+  
+    def edit
+      if params[:author_id]
+        author = Author.find_by(id: params[:author_id])
+        if author.nil?
+          redirect_to authors_path, alert: "Author not found."
+        else
+          @post = author.posts.find_by(id: params[:id])
+          redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+        end
+      else
+        @post = Post.find(params[:id])
+      end
+    end 
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :author_id)
   end
 end
